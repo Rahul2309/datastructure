532426a7a7c596741b097a30c63ee97f17514a85
Rahul C H <Rahul C H@DESKTOP-BO1BFIA>
2019-10-15 16:18:32 +0530

    new

--- My/src/com/my/linkedlist/Doublylinkedlist.java
+++ My/src/com/my/linkedlist/Doublylinkedlist.java
@@ -1,4 +1,5 @@
 package com.my.linkedlist;
+import java.util.*;
 
 public class Doublylinkedlist {
 	Node head;
@@ -22,29 +23,65 @@ public void push (int new_data) {
 		
 	}
 	head= new_node ;
-	
 }
-public void print(Node node) {
-	Node last = null;
-	 System.out.println("Traversal in forward Direction"); 
-     while (node != null) { 
-         System.out.print(node.data + " "); 
-         last = node; 
-         node = node.next; 
+ public void append(int new_data) {
+	 Node new_node = new Node(new_data);
+	 if(head == null) {
+		 new_node.prev = null;
+		 
+		 head = new_node;
+		 return;
+	 }
+	  new_node.next = null;
+	  Node last = head.next;
+	  while (last.next != null) {
+		  last = last.next;
+	  }
+	  last.next  =  new_node;
+	new_node.prev  =  last; 
+ }
+ public void insert(Node prev ,  int new_data) {
+	  
+	  if(prev == null) {
+		  System.out.println("Error");
+		  return;
+	  }
+	  Node new_node =  new Node(new_data);
+	  new_node.next =  prev.next;
+	  prev.next = new_node;
+	  new_node = prev; 
+	  if(new_node.next != null) {
+		  new_node.next.prev =  new_node;
+	  }
+ }
+public void print() {
+	Node node1 = head;
+	System.out.println("Traversal in forward Direction"); 
+     while (node1 != null) { 
+         System.out.print(node1.data + " "); 
+         node1 = node1.next; 
      } 
-	while(last != null) {
-		System.out.println(last.data+" ");
-	last = last.prev;
-	}
+	
 		
 }
+
 	public static void main(String[] args) {
+		Scanner  s = new Scanner(System.in) ;
+		int n = s.nextInt();
+	
 	Doublylinkedlist l =  new Doublylinkedlist();
 	l.push(4);
-	l.push(9);
-	l.push(4);
-	l.push(2);
-    l.print(l.head);
+
+	for(int i =0; i<n;i++) {
+		l.push(s.nextInt());
+	}
+	l.append(456);
+	l.append(647);
+	l.append(562);
+	l.append(9984);
+	l.insert(l.head.next, 5757344);
+	
+    l.print();
 	
 	}
 
